40f6375dfb3cf62ec12fe001376559c957828fb8
davidvieralopez@gmail.com <davidvieralopez@gmail.com>
2022-10-04 14:15:25 +0200

    changes demo/Entrepreneur 04/10/2022

---
+++ demo-backend/src/main/java/com/example/demo/dto/EntrepreneurDTO.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+
+public class EntrepreneurDTO {
+
+	int idEntrepreneur;
+	String firstName;
+	String lastName;
+	String email;
+	String idProfile;
+	String linkedinProfile;
+	
+}
