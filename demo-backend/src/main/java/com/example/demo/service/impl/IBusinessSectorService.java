b37bd080bc88ff0ad84bbcf4963d11905b0d6c02
davidvieralopez@gmail.com <davidvieralopez@gmail.com>
2022-10-24 13:25:50 +0200

    delete class

---
+++ demo-backend/src/main/java/com/example/demo/service/impl/IBusinessSectorService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.BusinessSector;
+import com.example.demo.service.GenericService;
+
+public interface IBusinessSectorService extends GenericService<BusinessSector, Integer> {
+
+
+}
