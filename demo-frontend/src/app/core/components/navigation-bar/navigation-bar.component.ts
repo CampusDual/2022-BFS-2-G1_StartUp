b24f1ca8fdf5558b8a7990ee9ed61f6ba186dd53
Martín <83639256+martin-mouzo-imatia@users.noreply.github.com>
2022-11-03 13:33:49 +0100

    Fix nav-bar and aesthetic enhancements

--- demo-frontend/src/app/core/components/navigation-bar/navigation-bar.component.ts
+++ demo-frontend/src/app/core/components/navigation-bar/navigation-bar.component.ts
@@ -7,18 +7,21 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.scss']
+  styleUrls: ['./navigation-bar.component.scss'],
 })
 export class NavigationBarComponent {
-
   @Output() toggleSidenav = new EventEmitter<void>();
 
   private returnUrl = '/';
   selectedLanguage = this.translateService.currentLang;
   userName: string;
 
-  constructor(private authService: AuthService, private router: Router, private logger: LoggerService,
-    private translateService: TranslateService) {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private logger: LoggerService,
+    private translateService: TranslateService
+  ) {
     this.userName = authService.getUserName();
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
@@ -26,21 +29,5 @@ export class NavigationBarComponent {
         this.logger.info('NavigationBarComponent returnUrl: ' + this.returnUrl);
       }
     });
-  }
-
-  logout() {
-    this.authService.logout();
-    // Redirect the user
-    this.router.navigateByUrl('/login');
-    localStorage.setItem('close_session', '1');
-    localStorage.setItem('close_session_language', this.translateService.currentLang);
-    setTimeout(() => {
-      window.location.reload();
-    }, 100);
-  }
-
-  toogleLanguage(lang: string) {
-    this.selectedLanguage = lang;
-    this.translateService.use(lang);
   }
 }
